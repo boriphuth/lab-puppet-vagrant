310d1f43c35dc909cc4d1d4773a5d01979d87f00
jing-tw <mqjing@gmail.com>
2016-05-24 19:00:49 +0800

    add stdout prefix

--- bash/up.sh
+++ bash/up.sh
@@ -4,10 +4,13 @@ cd master
 vagrant up
 cd ..
 
+sleep 2
+
 cd node1
 vagrant up
 cd ..
 
-. ./01_setup_hostname.sh
-. ./02_node1.sh
-. ./03_master_accept.sh 
+
+. ./01_add_host.sh | sed "s/^/[01_add_host.sh] /"
+. ./02_node1.sh | sed "s/^/[02_node1.sh] /"
+. ./03_master_accept.sh | sed "s/^/[03_master_accept.sh] /"
