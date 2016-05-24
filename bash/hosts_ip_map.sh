09f972983b86f55b23f05f0645d5a8a80e4c27f6
jing-tw <mqjing@gmail.com>
2016-05-24 19:02:07 +0800

    remove dummy code

--- bash/hosts_ip_map.sh
+++ bash/hosts_ip_map.sh
@@ -5,7 +5,7 @@ HostIP=( ["master.smb.com"]="192.168.33.10" \
          ["node1.smb.com"]="192.168.33.11")
 
 # Remove old HOST IDENTIFICATION
-for node in "${!HostIP[@]}"
-do
-    ssh-keygen -f "~/.ssh/known_hosts" -R ${HostIP["$node"]}
-done
+#for node in "${!HostIP[@]}"
+#do
+#    ssh-keygen -f "~/.ssh/known_hosts" -R ${HostIP["$node"]}
+#done
